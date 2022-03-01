7327ce97e5d13cd80dbb46335ae116171c29cd65
ariasmei <69130076+ariasmei@users.noreply.github.com>
2022-03-01 09:04:06 -0500

    adding js interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hi Welcome to my Portfolio Website, i tried my best")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
